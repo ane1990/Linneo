4220639f065a33704db063ca57bc6f0e4a03afeb
ane1990 <a.ane90@gmail.com>
2013-06-29 15:29:20 +0200

    services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,10 @@
 class Service < ActiveRecord::Base
   attr_accessible :faq, :info, :name
+  def previous
+    Service.where(["id < ?", id]).last
+  end
+
+  def next
+    Service.where(["id > ?", id]).first
+  end
 end
